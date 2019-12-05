48e45bfca10b000697bb2f9f31c51b227625698b
Jean Paul Diaz <jdiazvi2@intercorp.com.pe>
2019-12-05 12:05:12 -0500

    feat: make a better background

--- canvas.js
+++ canvas.js
@@ -1,35 +1,37 @@
 const canvas = document.querySelector('#animation')
 let start = null
-canvas.width = window.innerWidth + 200
-canvas.height = window.innerHeight + 200
+canvas.width = window.innerWidth + 500
+canvas.height = window.innerHeight + 500
 
-const context = canvas.getContext('2d')
+var ctx = canvas.getContext('2d');
+ctx.save()
 let counter = 0
 function animate () {
     let x = Math.random() * (window.innerWidth + 400)
     let y = Math.random() * (window.innerHeight + 400)
-    context.beginPath()
-    context.arc(x, y, 30, 0, Math.PI * 3, false)
-    context.strokeStyle = 'blue'
+    ctx.beginPath()
+    ctx.arc(x, y, 30, 0, Math.PI * 3, false)
+    ctx.strokeStyle = 'blue'
 
-    var ctx = canvas.getContext('2d');
     let a = Math.random() * window.innerWidth
     let b = Math.random() * window.innerHeight
-    ctx.beginPath();
+    ctx.beginPath()
     ctx.lineTo(a+20, b+10);
     ctx.lineTo(a+40, b+30);
     ctx.lineTo(a+30, b+50);
     ctx.lineTo(a+10, b+50);
     ctx.lineTo(a+0, b+30);
     ctx.lineTo(a+20, b+10);
-    ctx.strokeStyle = 'green';
+    ctx.strokeStyle = '#0c661894';
     ctx.closePath();
     ctx.stroke();
     counter++
-    if (counter < 500) {
+    if (counter < 300) {
         requestAnimationFrame(animate)
     } else {
-
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        counter = 0
+        animate()
     }
 }
 function clean () {
